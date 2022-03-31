8f518412928cd19dc631fa6a89706a00b5db5ae2
Theodore Glavas <thglavas@outlook.com>
2022-03-31 11:25:58 -0400

    Restructured Model and App

--- Frontend/app.py
+++ Frontend/app.py
@@ -2,51 +2,18 @@ import io
 import os
 import sys
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
 from PIL import Image
 
 
-import numpy as np
-import tensorflow as tf
-from tensorflow import keras
-from sklearn.model_selection import train_test_split
-import json
-from music21 import *
-from music21 import converter, chord, note, stream, environment, instrument, duration
+
+from model import Model
 
 app = Flask(__name__)
 
-# model = Model()
-# model.load_state_dict(torch.load("weights.pth"))
-model = tf.keras.models.load_model('saved_model/Model 5')
+model = Model()
 
-#Setting up the corpus
-with open('Corpus.json','r') as f:
-    Corpus  = json.load(f)
-symb = sorted(list(set(Corpus)))
-L_corpus = len(Corpus) #length of corpus
-L_symb = len(symb) #length of total unique characters
-#Building dictionary to access the vocabulary from indices and vice versa
-mapping = dict((c, i) for i, c in enumerate(symb))
-reverse_mapping = dict((i, c) for i, c in enumerate(symb))
 
-length = 40
-features = []
-targets = []
-for i in range(0, L_corpus - length, 1):
-    feature = Corpus[i:i + length]
-    target = Corpus[i + length]
-    features.append([mapping[j] for j in feature])
-    targets.append(mapping[target])
-    
-    
-L_datapoints = len(targets)
-
-X = (np.reshape(features, (L_datapoints, length, 1)))/ float(L_symb)
-# one hot encode the output variable
-y = keras.utils.to_categorical(targets) 
-
-X_train, X_seed, y_train, y_seed = train_test_split(X, y, test_size=0.2, random_state=42)
 
 @app.route("/")
 def index():
@@ -54,73 +21,13 @@ def index():
 
 @app.route("/selectionMade", methods=["POST"])
 def data():
-    """Generate music based on selection"""
-    selection = request.data
-    seed = X_seed[selection]
+    midiFile = model.predict(request)
+    print("request successful")
+    return True
     
-    Music_notes, Melody = melody_generator(80,seed)
-    Melody.write('midi','Melody_Generated.mid')
+@app.route('/download', methods=["FETCH"])
+def dowload():
+    return send_file('Melody_Generated.mid', as_attachment = True)
 
-
-
-    return None
-
-def melody_generator(Note_Count, seed):
-    """"Melody Generator"""
-    
-    Music = ""
-    Notes_Generated=[]
-    for i in range(Note_Count):
-        seed = seed.reshape(1,length,1)
-        prediction = model.predict(seed, verbose=0)[0]
-        prediction = np.log(prediction) / 1.0 #diversity
-        exp_preds = np.exp(prediction)
-        prediction = exp_preds / np.sum(exp_preds)
-        index = np.argmax(prediction)
-        index_N = index/ float(L_symb)   
-        Notes_Generated.append(index)
-        Music = [reverse_mapping[char] for char in Notes_Generated]
-        seed = np.insert(seed[0],len(seed[0]),index_N)
-        seed = seed[1:]
-        if i!=0 and i%80 == 0:
-            seed = X_seed[np.random.randint(0,len(X_seed)-1)]
-
-    #Now, we have music in form or a list of chords and notes and we want to be a midi file.
-    Melody = chords_n_notes(Music)
-    Melody_midi = stream.Stream(Melody)   
-    return Music,Melody_midi
-
-def chords_n_notes(Snippet):
-    """show notes and chords"""
-    Melody = []
-    offset = 0.0
-    for i in Snippet:
-        #If it is chord
-        if ("$" in i ):
-            i = i[1:]
-            dur_and_chord = i.split(",")
-            chord_notes = dur_and_chord[1].split(".") #Seperating the notes in chord
-            notes = [] 
-            for j in chord_notes:
-                inst_note=int(j)
-                note_snip = note.Note(inst_note)
-                note_snip.duration = duration.Duration(float(dur_and_chord[0]))
-                notes.append(note_snip)
-            chord_snip = chord.Chord(notes)
-            chord_snip.duration = duration.Duration(float(dur_and_chord[0]))
-            #chord_snip.offset = offset
-            Melody.append(chord_snip)
-            #offset += float(chord_snip.quarterLength)
-
-        # pattern is a note
-        else: 
-            dur_and_pitch = i.split(",")
-            note_snip = note.Note(int(dur_and_pitch[1]))
-            note_snip.duration = duration.Duration(float(dur_and_pitch[0]))
-            #note_snip.offset = offset
-            Melody.append(note_snip)
-            #offset += float(note_snip.quarterLength)
-        # increase offset each iteration so that notes do not stack
-        
-    Melody_midi = stream.Stream(Melody)
-    return Melody_midi
+if __name__ == '__main__':
+   app.run()
--- Frontend/model.py
+++ Frontend/model.py
@@ -1,58 +1,121 @@
-import torch.nn as nn
-import torchvision.transforms as transforms
-from collections import OrderedDict
+import io
+import os
+import sys
 
-transformation = transforms.Compose(
-    [transforms.Resize((32, 32)), transforms.ToTensor()]
-)
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from sklearn.model_selection import train_test_split
+import json
+import music21
+from music21 import *
+from music21 import converter, chord, note, stream, environment, instrument, duration
 
 
-class Model(nn.Module):
+
+class Model:
     """
-    Input - 1x32x32
-    C1 - 6@28x28 (5x5 kernel)
-    tanh
-    S2 - 6@14x14 (2x2 kernel, stride 2) Subsampling
-    C3 - 16@10x10 (5x5 kernel, complicated shit)
-    tanh
-    S4 - 16@5x5 (2x2 kernel, stride 2) Subsampling
-    C5 - 120@1x1 (5x5 kernel)
-    F6 - 84
-    tanh
-    F7 - 10 (Output)
+    
     """
 
     def __init__(self):
-        super(Model, self).__init__()
+     path = os.getcwd()
+     
+     filepath = os.path.join(path[:len(path)-9],"saved_model\\Model 4")
+     self.model = tf.keras.models.load_model(filepath)   
 
-        self.convnet = nn.Sequential(
-            OrderedDict(
-                [
-                    ("c1", nn.Conv2d(1, 6, kernel_size=(5, 5))),
-                    ("relu1", nn.ReLU()),
-                    ("s2", nn.MaxPool2d(kernel_size=(2, 2), stride=2)),
-                    ("c3", nn.Conv2d(6, 16, kernel_size=(5, 5))),
-                    ("relu3", nn.ReLU()),
-                    ("s4", nn.MaxPool2d(kernel_size=(2, 2), stride=2)),
-                    ("c5", nn.Conv2d(16, 120, kernel_size=(5, 5))),
-                    ("relu5", nn.ReLU()),
-                ]
-            )
-        )
+     with open(os.path.join(path[:len(path)-9],'Corpus.json'),'r') as f:
+         self.Corpus  = json.load(f)
+     symb = sorted(list(set(self.Corpus)))
+     L_corpus = len(self.Corpus) #length of corpus
+     self.L_symb = len(symb) #length of total unique characters
+     #Building dictionary to access the vocabulary from indices and vice versa
+     mapping = dict((c, i) for i, c in enumerate(symb))
+     self.reverse_mapping = dict((i, c) for i, c in enumerate(symb))
 
-        self.fc = nn.Sequential(
-            OrderedDict(
-                [
-                    ("f6", nn.Linear(120, 84)),
-                    ("relu6", nn.ReLU()),
-                    ("f7", nn.Linear(84, 10)),
-                    ("sig7", nn.LogSoftmax(dim=-1)),
-                ]
-            )
-        )
+     self.length = 40
+     features = []
+     targets = []
+     for i in range(0, L_corpus - self.length, 1):
+         feature = self.Corpus[i:i + self.length]
+         target = self.Corpus[i + self.length]
+         features.append([mapping[j] for j in feature])
+         targets.append(mapping[target])
+        
+        
+     L_datapoints = len(targets)
 
-    def forward(self, img):
-        output = self.convnet(img)
-        output = output.view(img.size(0), -1)
-        output = self.fc(output)
-        return output
+     X = (np.reshape(features, (L_datapoints, self.length, 1)))/ float(self.L_symb)
+     # one hot encode the output variable
+     y = keras.utils.to_categorical(targets) 
+
+     X_train, self.X_seed, y_train, y_seed = train_test_split(X, y, test_size=0.2, random_state=42)
+        
+
+    def melody_generator(self, Note_Count):
+     """"Melody Generator"""
+     seed = self.X_seed[np.random.randint(0,len(self.X_seed)-1)]
+     Music = ""
+     Notes_Generated=[]
+     for i in range(Note_Count):
+         seed = seed.reshape(1,self.length,1)
+         prediction = self.model.predict(seed, verbose=0)[0]
+         #prediction = np.log(prediction) / 1.0 #diversity
+         exp_preds = np.exp(prediction)
+         prediction = exp_preds / np.sum(exp_preds)
+         index = np.argmax(prediction)
+         index_N = index/ float(self.L_symb)   
+         Notes_Generated.append(index)
+         Music = [self.reverse_mapping[char] for char in Notes_Generated]
+         seed = np.insert(seed[0],len(seed[0]),index_N)
+         seed = seed[1:]
+         if i!=0 and i%80 == 0:
+             seed = self.X_seed[np.random.randint(0,len(self.X_seed)-1)]
+
+     #Now, we have music in form or a list of chords and notes and we want to be a midi file.
+     Melody = self.chords_n_notes(Music)
+     Melody_midi = stream.Stream(Melody)   
+     return Music,Melody_midi
+    
+
+    def chords_n_notes(self, Snippet):
+     """show notes and chords"""
+     Melody = []
+     offset = 0.0
+     for i in Snippet:
+         #If it is chord
+         if ("$" in i ):
+             i = i[1:]
+             dur_and_chord = i.split(",")
+             chord_notes = dur_and_chord[1].split(".") #Seperating the notes in chord
+             notes = [] 
+             for j in chord_notes:
+                 inst_note=int(j)
+                 note_snip = note.Note(inst_note)
+                 note_snip.duration = duration.Duration(float(dur_and_chord[0]))
+                 notes.append(note_snip)
+             chord_snip = chord.Chord(notes)
+             chord_snip.duration = duration.Duration(float(dur_and_chord[0]))
+             #chord_snip.offset = offset
+             Melody.append(chord_snip)
+             #offset += float(chord_snip.quarterLength)
+
+         # pattern is a note
+         else: 
+             dur_and_pitch = i.split(",")
+             note_snip = note.Note(int(dur_and_pitch[1]))
+             note_snip.duration = duration.Duration(float(dur_and_pitch[0]))
+             #note_snip.offset = offset
+             Melody.append(note_snip)
+             #offset += float(note_snip.quarterLength)
+         # increase offset each iteration so that notes do not stack
+        
+     Melody_midi = stream.Stream(Melody)
+     return Melody_midi
+    
+    def predict(self, request):
+        Music_notes, Melody = self.melody_generator(80)
+        Melody.write('midi','Melody_Generated.mid')
+        return 'Melody_Generated.mid'
+        
+
